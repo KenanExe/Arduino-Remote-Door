3ea870cb0da4d56d16ae8fdfbb99101120572ad7
KenanExe <99500843+KenanExe@users.noreply.github.com>
2025-05-19 15:24:16 +0300

    Update Main.ino

--- Arduino/Main.ino
+++ Arduino/Main.ino
@@ -3,22 +3,27 @@
 #include <LiquidCrystal.h>
 #include <Servo.h>
 
-int ledPinRed = 3;     // Kırmızı
-int ledPinYellow = 4;  // Sarı
-int ledPinGreen = 5;   // Yeşil
+
+int ledPinRed = 3;     // Red
+int ledPinYellow = 4;  // Yellow 
+int ledPinGreen = 5;   // Green
 int irPin = 6;
 int servoPin = 9;
 int potPin = A0;
-
+int x = 0;
 IRrecv irrecv(irPin);
 decode_results results;
 LiquidCrystal lcd(0, 1, 2, 7, 11, 12);
 Servo motor;
 
 void setup() {
+
+  pinMode(13, OUTPUT); //arduino uno LED
+  //pinmode(2, OUTPUT); //esp LED
+
   motor.attach(servoPin);
   lcd.begin(16, 2);
-  lcd.print("( ͡• ͜ʖ ͡•)");
+  lcd.print(":D");
   irrecv.enableIRIn();
   pinMode(ledPinRed, OUTPUT);
   pinMode(ledPinYellow, OUTPUT);
@@ -26,7 +31,9 @@ void setup() {
 }
 
 void loop() {
-  
+  digitalWrite(13, HIGH); //uno LED
+  //digitalWrite(2, HIGH); //esp LED
+
   int reading = analogRead(potPin);
   int angle = map(reading, 0, 1023, 0, 180);
 
@@ -36,25 +43,33 @@ void loop() {
 
   if (angle == 0) {
     lcd.setCursor(0, 1);
-    lcd.print("Kilitli");
+    lcd.print("Lock");
     digitalWrite(ledPinRed, HIGH);
     digitalWrite(ledPinYellow, LOW);
     digitalWrite(ledPinGreen, LOW);
   } else if (angle == 180) {
     lcd.setCursor(0, 1);
-    lcd.print("Kilit acildi");
+    lcd.print("Unlock");
     digitalWrite(ledPinRed, LOW);
     digitalWrite(ledPinYellow, LOW);
     digitalWrite(ledPinGreen, HIGH);
   } else {
     lcd.setCursor(0, 1);
-    lcd.print("Komut Bekleniyor");
+    lcd.print("Waiting code...");
     digitalWrite(ledPinRed, LOW);
     digitalWrite(ledPinYellow, HIGH);
     digitalWrite(ledPinGreen, LOW);
   }
 
-  delay(200);
+  digitalWrite(13, LOW); //uno LED
+  //digitalWrite(2, LOW); //esp LED
+  delay(220); //I think it's very optimize delay
+//  if u using esp, u can reboot system for it. (some times it's stuck)
+//   if (x >= 100) {
+//    x = 0;
+//    ESP.restart();
+//  }
+
 }
 
 void unlock() {
